99793fc300d4ae15107dd4d960379070eeea2fe4
Kofi Dwomoh <kofi.dwomoh-ababio@smartsapp.com>
2024-12-14 11:27:19 +0000

    en: updates

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins ENV["CORS_ORIGINS"].split(",").map { |origin| origin.strip }
+      origins "http://localhost:3030,cool-boba-e226e1.netlify.app,incident-tracker.sentigroup.com,https://incident-tracker.sentigroup.com".split(",").map { |origin| origin.strip }
       resource "*",
                headers: :any,
                methods: %i[get post put patch delete options head]
